4060809fdc054acbf8cb911cad147faf94fe3037
Andrew Darr <kildar07@gmail.com>
2022-08-10 08:21:34 -0500

    commit tree d3f8fa629cec537137fd2284af5a146281ccb1f0

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// declared in global
+var customerName = ('bob');
+
+// declared global
+const leastFavoriteCustomer = ('john');
+
+// changes customerName to uppercase
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+leastFavoriteCustomer = 'bobby';
+}
